d758fb097eb5a3ac50bf286543ce8ed260b05b7f
goloblyakonstantin <goloblyakonstantin@gmail.com>
2020-08-01 10:42:37 +0300

    меню

--- www/php/header.php
+++ www/php/header.php
@@ -3,12 +3,17 @@
     <img src="img/logo/logo_menu.svg" alt="лого в меню">
     <nav>
       <ul>
-        <li>каталог дверей</li>
-        <li>каталог отделки</li>
-        <li>фурнитура</li>
-        <li>установка</li>
-        <li>услуги</li>
-        <li>контакты</li>
+        <li><a href=""><p>каталог дверей</p></a></li>
+        <li><a href=""><p>каталог отделки</p></a>
+          <ul>
+            <li><a href=""><p>декор. панели</p></a></li>
+            <li><a href=""><p>доборы и наличники</p></a></li>
+          </ul>
+        </li>
+        <li><a href=""><p>фурнитура</p></a></li>
+        <li><a href=""><p>установка</p></a></li>
+        <li><a href=""><p>услуги</p></a></li>
+        <li><a href=""><p>контакты</p></a></li>
       </ul>
     </nav>
     <button type="button" name="button">заказать звонок</button>
