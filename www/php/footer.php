364a5ccd9d5054c7f09152f56e5efd167f0829d2
goloblyakonstantin <goloblyakonstantin@gmail.com>
2020-08-01 10:47:49 +0300

    футер

--- www/php/footer.php
+++ www/php/footer.php
@@ -1,1 +1,14 @@
-
+<footer>
+  <div class="footer-div_0">
+    <div>
+      <h1>Остались вопросы?</h1>
+      <h1>Хотите вызвать замерщика?</h1>
+      <p>Заполните форму и наши менеджеры свяжутся с вами в ближайшее время.</p>
+    </div>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="" value="" placeholder="имя">
+      <input type="text" name="" value="" placeholder="телефон">
+      <button type="submit" name="button">Заказать звонок</button>
+    </form>
+  </div>
+</footer>
