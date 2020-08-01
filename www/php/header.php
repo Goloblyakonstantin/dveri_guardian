c8918fb190f5a4e164e84afc3676d47db9ada0c9
goloblyakonstantin <goloblyakonstantin@gmail.com>
2020-08-01 10:39:10 +0300

    меню хедер

---
+++ www/php/header.php
@@ -0,0 +1,16 @@
+<header>
+  <menu>
+    <img src="img/logo/logo_menu.svg" alt="лого в меню">
+    <nav>
+      <ul>
+        <li>каталог дверей</li>
+        <li>каталог отделки</li>
+        <li>фурнитура</li>
+        <li>установка</li>
+        <li>услуги</li>
+        <li>контакты</li>
+      </ul>
+    </nav>
+    <button type="button" name="button">заказать звонок</button>
+  </menu>
+</header>
